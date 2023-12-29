618a2811b458bcdf64a5024b5c5fff98b7c15388
Mohammed Akbar Ali <96139030+hafizakbar19@users.noreply.github.com>
2023-12-29 16:58:39 +0500

    time function added

--- script.js
+++ script.js
@@ -5,3 +5,12 @@ document.getElementById('size').innerHTML = `Size: ${width} X ${height}`;
 document.getElementById('asize').innerHTML = `Availabel size: ${screen.availWidth} X ${screen.availHeight}`;
 document.getElementById('color').innerHTML = `Screen color depth: ${screen.colorDepth}`;
 document.getElementById('pixel').innerHTML = `Screen pixel depth: ${screen.pixelDepth}`;
+
+let timer = () => {
+    let d = new Date();
+    document.getElementById('time').innerHTML = `Time: ${d.toLocaleTimeString()}`;
+};
+
+timer();
+
+setInterval(timer, 1000);
